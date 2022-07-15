181f908d4c76e4199fd74b0ba9d314fd8fa2b6b7
jlsantia <jlsantia@ucsc.edu>
2022-07-14 18:48:24 -0700

    Started writing python code to turn dataframe into SQL database.

--- programs/cloud_db.py
+++ programs/cloud_db.py
@@ -1,7 +1,11 @@
+from scrape_makinex import products_df
 from sql2py import create_server_connection
 from sql2py import create_database
 from sql2py import create_db_connection
 from sql2py import execute_query
 from sql2py import read_query
 from sql2py import execute_list_query
+
+#create the dataframe
+df = products_df()
 
