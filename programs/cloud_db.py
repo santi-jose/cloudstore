2ed6491ae24a3b6980285639bcb7db89e92f5334
jlsantia <jlsantia@ucsc.edu>
2022-07-15 15:43:50 -0700

    Created cloudstore database using dataframe created from scrape_makinex.py.

--- programs/cloud_db.py
+++ programs/cloud_db.py
@@ -9,3 +9,39 @@ from sql2py import execute_list_query
 #create the dataframe
 df = products_df()
 
+#establish connection to mySQL server
+connection = create_server_connection("localhost", "root", "sqlV0idprogr@m")
+
+#create database
+create_database_query = "CREATE DATABASE cloudstore"
+create_database(connection, create_database_query)
+
+#connect to cloudstore database
+connection = create_db_connection("localhost", "root", "sqlV0idprogr@m", "cloudstore")
+
+#assign SQL query command to create tables
+create_product_table = """
+CREATE TABLE PRODUCTS (
+    product_name VARCHAR(50) NOT NULL,
+    price VARCHAR(20),
+    more_info VARCHAR(100) NOT NULL
+);
+"""
+
+execute_query(connection, create_product_table)
+
+sql = """
+    INSERT INTO PRODUCTS (product_name, price, more_info)
+    VALUES (%s, %s, %s)
+"""
+
+val = []
+
+for index, row in df.iterrows():
+    # print(row['Product Name'], row['Price'], row['More Info'])
+    val.append([row['Product Name'], row['Price'], row['More Info']])
+    print(val[index])
+    #execute_list_query(connection, sql, [row['Produt Name'], row['Price'], row['More Info']])
+    print('')
+
+execute_list_query(connection, sql, val)
