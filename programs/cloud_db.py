6639f68b55f36a0093f11650b6b9a57398c9519a
jlsantia <jlsantia@ucsc.edu>
2022-07-14 18:26:45 -0700

    Setting repository up to be able to create database for products found with webscraping.

---
+++ programs/cloud_db.py
@@ -0,0 +1,7 @@
+from sql2py import create_server_connection
+from sql2py import create_database
+from sql2py import create_db_connection
+from sql2py import execute_query
+from sql2py import read_query
+from sql2py import execute_list_query
+
