c47fd2aadbd2a450527f0c3b2e7f841c348abfb4
jlsantia <jlsantia@ucsc.edu>
2022-07-15 17:21:33 -0700

    Python program used to successfully create database on MySQL server from dataframe made with scrape_makinex.py program.

--- programs/cloud_db.py
+++ programs/cloud_db.py
@@ -24,22 +24,24 @@ create_product_table = """
 CREATE TABLE PRODUCTS (
     product_name VARCHAR(50) NOT NULL,
     price VARCHAR(20),
-    more_info VARCHAR(100) NOT NULL
+    category VARCHAR(50),
+    more_info VARCHAR(100) NOT NULL,
+    img_link VARCHAR(100) NOT NULL
 );
 """
 
 execute_query(connection, create_product_table)
 
 sql = """
-    INSERT INTO PRODUCTS (product_name, price, more_info)
-    VALUES (%s, %s, %s)
+    INSERT INTO PRODUCTS (product_name, price, category, more_info, img_link)
+    VALUES (%s, %s, %s, %s, %s)
 """
 
 val = []
 
 for index, row in df.iterrows():
     # print(row['Product Name'], row['Price'], row['More Info'])
-    val.append([row['Product Name'], row['Price'], row['More Info']])
+    val.append([row['Product Name'], row['Price'], row['Category'], row['More Info'], row['Image Link']])
     print(val[index])
     #execute_list_query(connection, sql, [row['Produt Name'], row['Price'], row['More Info']])
     print('')
