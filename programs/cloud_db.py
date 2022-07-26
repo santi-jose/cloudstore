999e5089c62ffab0e7e50df2189ef2a3d9776c9f
jlsantia <jlsantia@ucsc.edu>
2022-07-26 14:56:44 -0700

    Changed database to store table of products across all scraped websites.

--- programs/cloud_db.py
+++ programs/cloud_db.py
@@ -14,131 +14,71 @@ from sql2py import execute_list_query
 connection = create_server_connection("localhost", "root", "sqlV0idprogr@m")
 
 #create database
-# create_database_query = "CREATE DATABASE cloudstore"
+create_database_query = "CREATE DATABASE cloudstore"
 # create_database(connection, create_database_query)
 
 #connect to cloudstore database
 connection = create_db_connection("localhost", "root", "sqlV0idprogr@m", "cloudstore")
 
-# CREATE MAKINEX TABLE IN CLOUDSTORE DATABASE
-#create the dataframe
-# df = makinex_products_df()
-
 #assign SQL query command to create tables
-create_makinex_table = """
-CREATE TABLE MAKINEX (
-    product_name VARCHAR(50) NOT NULL,
+create_products_table = """
+CREATE TABLE PRODUCTS (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(100) NOT NULL,
     price VARCHAR(20),
-    manufacturer VARCHAR(50),
-    category VARCHAR(50),
-    more_info VARCHAR(100) NOT NULL,
-    img_link VARCHAR(100) NOT NULL,
-    timestamp VARCHAR(50)
+    manufacturer VARCHAR(100),
+    category VARCHAR(100),
+    more_info VARCHAR(200) NOT NULL,
+    img_link VARCHAR(200) NOT NULL,
+    timestamp VARCHAR(50) NOT NULL
 );
 """
 
-# execute_query(connection, create_makinex_table)
+execute_query(connection, create_products_table)
 
 sql = """
-    INSERT INTO MAKINEX (product_name, price, manufacturer, category, more_info, img_link, timestamp)
-    VALUES (%s, %s, %s, %s, %s, %s, %s)
+    INSERT INTO PRODUCTS (product_id, product_name, price, manufacturer, category, more_info, img_link, timestamp)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 """
 
 val = []
 
-# for index, row in df.iterrows():
-#     val.append([row['Product Name'], row['Price'], row['Manufacturer'], row['Category'], row['More Info'], row['Image Link'], row['Time']])
+i = 1
 
-# execute_list_query(connection, sql, val)
+# CREATE MAKINEX TABLE IN CLOUDSTORE DATABASE
+#create the dataframe
+makinex_df = makinex_products_df()
+makinex_df.replace({r'[^\x00-\x7F]+':''}, regex=True, inplace=True)
+
+for index, row in makinex_df.iterrows():
+     val.append([str(i), row['Product Name'], row['Price'], row['Manufacturer'], row['Category'], row['More Info'], row['Image Link'], row['Time']])
+     i = i + 1
 
 # CREATE VICTORY TABLE IN CLOUDSTORE DATABASE
 # Make dataframe from victory products webpage
-# victory_df = victory_products_df()
+victory_df = victory_products_df()
+victory_df.replace({r'[^\x00-\x7F]+':''}, regex=True, inplace=True)
 
-# victory product table
-create_victory_table = """
-CREATE TABLE VICTORY (
-    product_name VARCHAR(100) NOT NULL,
-    price VARCHAR(20),
-    manufacturer VARCHAR(50),
-    category VARCHAR(100),
-    more_info VARCHAR(200) NOT NULL,
-    img_link VARCHAR(200) NOT NULL,
-    timestamp VARCHAR(50)
-);
-"""
-
-# execute_query(connection, create_victory_table)
-
-sql = """
-    INSERT INTO VICTORY (product_name, price, manufacturer, category, more_info, img_link, timestamp)
-    VALUES (%s, %s, %s, %s, %s, %s, %s)
-"""
-
-victory_val = []
-
-# for index, row in victory_df.iterrows():
-#     victory_val.append([row['Product Name'], row['Price'], row['Manufacturer'], row['Category'], row['More Info'], row['Image Link'], row['Time']])
-
-# execute_list_query(connection, sql, victory_val)
+for index, row in victory_df.iterrows():
+     val.append([str(i), row['Product Name'], row['Price'], row['Manufacturer'], row['Category'], row['More Info'], row['Image Link'], row['Time']])
+     i = i + 1
 
 # CREATE TOOLOTS TABLE IN CLOUDSTORE DATABASE
 # make dataframe from toolots product webpage
-# toolots_df = toolots_products_df()
+toolots_df = toolots_products_df()
+toolots_df.replace({r'[^\x00-\x7F]+':''}, regex=True, inplace=True)
 
-# toolots product table
-create_toolots_table = """
-CREATE TABLE TOOLOTS (
-    product_name VARCHAR(150) NOT NULL,
-    price VARCHAR(20),
-    manufacturer VARCHAR(50),
-    category VARCHAR(100),
-    more_info VARCHAR(200) NOT NULL,
-    img_link VARCHAR(200) NOT NULL,
-    timestamp VARCHAR(50)
-);
-"""
-
-# execute_query(connection, create_toolots_table)
-
-sql = """
-    INSERT INTO TOOLOTS (product_name, price, manufacturer, category, more_info, img_link, timestamp)
-    VALUES (%s, %s, %s, %s, %s, %s, %s)
-"""
-
-# toolots_val = []
-
-# for index, row in toolots_df.iterrows():
-#     toolots_val.append([row['Product Name'], row['Price'], row['Manufacturer'], row['Category'], row['More Info'], row['Image Link'], row['Time']])
-
-# execute_list_query(connection, sql, toolots_val)
+for index, row in toolots_df.iterrows():
+     val.append([str(i), row['Product Name'], row['Price'], row['Manufacturer'], row['Category'], row['More Info'], row['Image Link'], row['Time']])
+     i = i + 1
 
 # CREATE TITAN MACHINARY TABLE IN CLOUDSTORE DATABASE
 # make dataframe from toolots product webpage
 titan_df = titanmachinery_products_df()
-
-# toolots product table
-create_titan_table = """
-CREATE TABLE TITAN (
-    product_name VARCHAR(150) NOT NULL,
-    price VARCHAR(20),
-    manufacturer VARCHAR(50),
-    category VARCHAR(100),
-    more_info VARCHAR(200) NOT NULL,
-    img_link VARCHAR(200) NOT NULL
-);
-"""
-
-execute_query(connection, create_titan_table)
-
-sql = """
-    INSERT INTO TITAN (product_name, price, manufacturer, category, more_info, img_link)
-    VALUES (%s, %s, %s, %s, %s, %s)
-"""
-
-titan_val = []
+titan_df.replace({r'[^\x00-\x7F]+':''}, regex=True, inplace=True)
 
 for index, row in titan_df.iterrows():
-    titan_val.append([row['Product Name'], row['Price'], row['Manufacturer'], row['Category'], row['More Info'], row['Image Link']])
+    val.append([str(i), row['Product Name'], row['Price'], row['Manufacturer'], row['Category'], row['More Info'], row['Image Link'], row['Time']])
+    i = i + 1
 
-execute_list_query(connection, sql, titan_val)
+execute_list_query(connection, sql, val)
